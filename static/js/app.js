24b9ea6e25b218e1d8754e53ccdd20c18ee26e77
Pragathi Madushinie <prmadushini@gmail.com>
2019-08-07 16:05:00 -0500

    Error Correction

--- static/js/app.js
+++ static/js/app.js
@@ -1,57 +1,71 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
-// target the place "table" where data will be appended into
-var tbody = d3.select("#target-table-display");
-// select the "submit button" & "input field"
-var submitButton = d3.select("#filter-btn-date");
-var inputField = d3.select("#datetime");
+var tbody = d3.select("tbody");
+
+var ufosighting=data;     
 
 
-
-data.forEach(obj => {
-    // for each "element" in the object create a row
-    var tRow = tbody.append("tr");
-    //below "object" becomes the targetet array (element)
-    Object.entries(obj).forEach(([key,value]) => {
-        // console.log(`The key is: ${key} and the value is: ${value}`);
-        var tData = tRow.append("td");
-        // adds the "value" to each row in the table
-        tData.text(value);
+ufosighting.forEach((ufodata) => {
+    var row = tbody.append("tr");
+    Object.entries(ufodata).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
     });
-});
+  });
+
+  
+
+  var filterbtn=d3.select("#filter-btn");
+  
+  filterbtn.on("click",function(){
+
+    var filteredData = ufosighting;
+
+    var inputDate= d3.select("#datetime").property("value");
+    var inputCity= d3.select("#City").property("value").toLowerCase().trim();
+    var inputState= d3.select("#State").property("value").toLowerCase().trim();
+    var inputCountry= d3.select("#Country").property("value").toLowerCase().trim();
+    var inputShape= d3.select("#Shape").property("value").toLowerCase().trim();
+    
+
+    var inputUser={ datetime:inputDate,
+                    city:inputCity,
+                    state:inputState,
+                    country:inputCountry,
+                    shape:inputShape
+                    }
+    
+
+    Object.entries(inputUser).forEach(([key, value]) => {
+      if(value===""){
+        delete  inputUser[key];
+      }   
+     });
 
 
+   //console.log(inputUser);
+    //console.log("Button Click Event");
+    //console.log("inputValue :",inputUser);
+    
+   
 
-//implementing fuction to "submit button"
-submitButton.on("click", function() {
-    // clears the data of the current table        
-    tbody.html("");
-    // stop the page from refresh
-    d3.event.preventDefault();
-    // print "You have just clicked the 'Filter Table' on console, for testing
-    console.log("You have just clicked the ' Date Time Filter Button'.");
-    // select the "input element" and get the raw html node
-    var inputField = d3.select("#datetime");
-    // get the value property of the "input" element 
-    var inputElement = inputField.property("value");
-    // print value in console
-    console.log(inputElement);
-    // use the "field input" to filter the data by "date" only
-    var inputTypeArray = data.filter(one => one.datetime === inputElement);   // var inputTypeArray = data.filter(one => one[chosen] === inputElement);
-    console.log(inputTypeArray)
+    filteredData = filteredData.filter(row => {
+      return Object.entries(inputUser).every(criteria => {
+        const key = criteria[0]
+        const value = criteria[1]
+        return row[key] === value
+      })
+    })
+    
+    var tableContent=d3.select("tbody");
 
     
-    // display in the html file (appends it at the end, after displaying all original data)
-    inputTypeArray.forEach((selection) => {
-        // for each "element" create a row
-        var row = tbody.append("tr");
-        //below "object" becomes the targetet array (element)
-        Object.entries(selection).forEach(([key,value]) => {
-            var cell = row.append("td");
-            // adds the "value" to each row in the table
-            cell.text(value);
-        });
-    });      
-});
+    tableContent.html("");
+
+    filteredData.forEach((fData) => {
+      var row = tbody.append("tr");
+      Object.entries(fData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+   });
